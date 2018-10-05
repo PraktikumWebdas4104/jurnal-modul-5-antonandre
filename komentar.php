d62472c1b2d8758f02548ef6705449f0ff0dfa0d
antonandre <43515471+antonandre@users.noreply.github.com>
2018-10-05 10:16:45 +0700

    Update komentar.php
    ANTONIUS ANDRE YUWONO PUTRANTO
    6701170015
    D3MI-41-04

--- komentar.php
+++ komentar.php
@@ -1,3 +1,18 @@
-<?php
+<form method="POST">
+	Komentar : <input type="text" name="komentar"><br>
+	<input type="submit" name="kirim" value="CEK">
+</form>
 
+<?php
+}
+if (isset($_POST['kirim'])){
+	
+	$komentar = $_POST['komentar'];
+	
+	if(str_word_count($komentar)>=5){
+		echo $komentar;
+		}else if(str_word_count($komentar)<5){
+		echo "Kata yang diinputkan kurang dari 5";
+		}
+}
 ?>
