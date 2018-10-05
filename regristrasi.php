00024abd6cbda6b4cd18fddb1909893c2a832342
antonandre <43515471+antonandre@users.noreply.github.com>
2018-10-05 09:57:26 +0700

    Update regristrasi.php
    ANTONIUS ANDRE YUWONO PUTRANTO
    6701170015
    D3MI-41-04

--- regristrasi.php
+++ regristrasi.php
@@ -5,6 +5,31 @@
 </head>
 <body>
 
+<form method="POST">
+	<table>
+		<tr>
+			<td>NIM</td>
+			<td>:</td>
+			<td><input type="number" name="nim" length="10"></td>
+		</tr>
+
+		<tr>
+			<td>Nama</td>
+			<td>:</td>
+			<td><input type="text" name="nama" length="25"></td>
+		</tr>
+
+		<tr>
+			<td>E-mail</td>
+			<td>:</td>
+			<td><input type="email" name="email"></td>
+		</tr>
+
+		<tr>
+			<td><input type="submit" name="kirim"></td>
+		</tr>		
+	</table>
+</form>
 
 
 </body>
